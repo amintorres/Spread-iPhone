4714477cd42c470f21b9e4aa43b4b181173014dc
Joseph Lin <josephlin0922@gmail.com>
2012-01-15 22:40:23 -0500

    Added image viewer

---
+++ Spread/ImageScrollView.h
@@ -0,0 +1,18 @@
+//
+//  ImageScrollView.h
+//  Spread
+//
+//  Created by Joseph Lin on 1/15/12.
+//  Copyright (c) 2012 Joseph Lin. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface ImageScrollView : UIScrollView <UIScrollViewDelegate>
+
+- (void)displayImage:(UIImage *)image;
+
+@end
